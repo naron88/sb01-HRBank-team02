ba23048107d6d82f0f14592a7482d67a823625f4
eunseobb <seobseob0810@naver.com>
2025-03-17 15:59:18 +0900

    refactor(backup): BackupController의 어노테이션을 Controller에서 RestController로 수정

--- src/main/java/com/practice/hrbank/controller/BackupController.java
+++ src/main/java/com/practice/hrbank/controller/BackupController.java
@@ -7,13 +7,13 @@ import java.time.Instant;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/backups")
 @RequiredArgsConstructor
 public class BackupController {
